c10af0732fc1b62507d9db93a6e1540b15b5764e
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-05 15:34:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// alert()
+
+// variable 3 dhoroner,
+
+// 1 .. Var -- ata amra used korbo nha   ,
+// 2 .. let -- kunu man poriboirton korle ai ta used korbo,
+// 3 .. const -- man poriborton na korle ai ta used korbo ,
