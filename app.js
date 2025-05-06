8655e15e9226c937d7aec3dfd89c8f5fe9cc0ea7
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-06 11:24:58 +0600

    add jhava

--- app.js
+++ app.js
@@ -5,3 +5,90 @@
 // 1 .. Var -- ata amra used korbo nha   ,
 // 2 .. let -- kunu man poriboirton korle ai ta used korbo,
 // 3 .. const -- man poriborton na korle ai ta used korbo ,
+
+//  ler variable diclaration
+
+// let name = "Golap"
+// console.log(name)
+// name = "Golap Raj"
+// console.log(name)
+
+// const variable diclaration
+
+// const name = "golap"
+// console.log(name)
+// name = "Golap Raj"
+// console.log(name)
+
+// if else condison
+const number = 20;
+// if(number < 10){
+//     console.log("iss")
+// }
+// else{
+//     console.log("Noooo")
+// }
+// if(number <= 20){
+//     console.log("iss")
+// }
+// else{
+//     console.log("Noooo")
+// if(number < 20 || number >19 ){
+//     console.log("iss")
+// }
+// else{
+//     console.log("Noooo")
+// }
+// if(number < 20 && number >19 ){
+//     console.log("iss")
+// }
+// else{
+//     console.log("Noooo")
+// }
+
+// Array diclaration
+
+// const friends = ["Golap", "Raj", "Basar", "Simul"];
+// for(let i = 0; i < friends.length; i++){
+//     console.log(friends[i])
+// }
+// for (let friend of friends){
+//     console.log(friend)
+// }
+// console.log(friends.slice(2,3))
+// console.log(friends.splice(2,3))
+// friends.push("Afsana")
+// friends.pop()
+// friends.unshift("Jibon")
+// friends.shift()
+// const allFriends = ["hhhh", ...friends]
+// console.log(allFriends)
+// console.log(friends)
+// console.log(friends[0])
+// console.log(friends.length)
+
+// function diclaration
+
+// function sum (a, b) {
+//     const result = a + b;
+//     console.log(result)
+
+// }
+// sum(11, 9)
+
+// function sum (a, b) {
+//     // console.log("OK")
+//     const sum = a + b;
+//     console.log(sum)
+// }
+// sum(10, 20)
+
+// Object diclaration
+const person = {
+    name : "Golap",
+    id : 11,
+    fatherName : "Abdullah"
+}
+
+console.log(person["name"])
+console.log(person.id)
